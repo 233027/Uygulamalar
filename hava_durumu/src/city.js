af356603707a938fb99b48a540833d9ee59487d9
esmadinc <nurdnc20@gmail.com>
2022-08-03 17:29:08 +0300

    yorum satırı silindi

--- hava_durumu/src/city.js
+++ hava_durumu/src/city.js
@@ -4,12 +4,6 @@ const City = ({city}) => {
     console.log(city)
   return (
     <div>
-      {/* <div>
-        <h1>{city.main.temp}</h1>
-        <h1>{city.name}</h1>
-        <h1>{city.weather[0].main}</h1>
-      </div> */}
-
       <div>
         <h1>{city.main.temp}</h1>
         <h1>{city.name}</h1>
